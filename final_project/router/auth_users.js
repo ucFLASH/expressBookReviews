31b44b65b976b7b5d2e2bd3f430fce8d2500aa94
ucFLASH <www.ucdash@gmail.com>
2023-07-06 20:34:05 -0400

    new user can now register and login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,29 +2,105 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
-
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (!token) {
+      return res.status(401).json({ message: 'Access token not found' });
+    }
+  
+    jwt.verify(token, 'secretKey', (err, user) => {
+      if (err) {
+        return res.status(403).json({ message: 'Invalid token' });
+      }
+      req.user = user;
+      next();
+    });
+  };
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  // Write code to check if the username is valid
+  // For example, you can check if the username exists in the users array
+  return users.some((user) => user.username === username);
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  // Write code to check if the username and password match the one we have in records
+  // For example, you can iterate over the users array and validate the credentials
+  return users.some((user) => user.username === username && user.password === password);
+};
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Only registered users can login
+regd_users.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Check if username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+
+  // Check if the credentials are authenticated
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  // Generate and sign the JWT token
+  const token = jwt.sign({ username }, "secretKey");
+
+  return res.status(200).json({ message: "Login successful", token });
 });
 
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+regd_users.put("/auth/review/:isbn", authenticateToken, (req, res) => {
+    const { isbn } = req.params;
+    const { review } = req.query;
+    const { username } = jwt.decode(req.headers.authorization.split(' ')[1]);
+  
+    let bookArray = Object.values(books);
+    let book = bookArray.find((b) => b.isbn === isbn);
+  
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+  
+    let reviewIndex = book.reviews.findIndex((r) => r.username === username);
+  
+    if (reviewIndex !== -1) {
+      // If the user has already posted a review, modify the existing review
+      book.reviews[reviewIndex].review = review;
+      return res.status(200).json({ message: "Review modified successfully" });
+    } else {
+      // If it's a new review, add it to the book's reviews array
+      book.reviews.push({ username, review });
+      return res.status(200).json({ message: "Review added successfully" });
+    }
+  });
+  
+ // Delete a book review
+ regd_users.delete("/auth/review/:isbn", authenticateToken, (req, res) => {
+    const { isbn } = req.params;
+  const { username } = jwt.decode(req.headers.authorization.split(' ')[1]);
 
+  let bookArray = Object.values(books);
+  let book = bookArray.find((b) => b.isbn === isbn);
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  let reviewIndex = book.reviews.findIndex((r) => r.username === username);
+
+  if (reviewIndex !== -1) {
+    // If the user has posted a review, remove it from the book's reviews array
+    book.reviews.splice(reviewIndex, 1);
+    return res.status(200).json({ message: "Review deleted successfully" });
+  } else {
+    return res.status(404).json({ message: "Review not found" });
+  }
+});
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,181 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+public_users.post("/register", (req, res) => {
+  const { username, password } = req.body;
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Check if username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  // Check if the username already exists
+  if (users.some(user => user.username === username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+
+  // Add the new user to the users array
+  users.push({ username, password });
+
+  return res.status(200).json({ message: "User registered successfully" });
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', async function (req, res) {
+    try {
+      // Creating a promise method. The promise will get resolved when the timer times out after 6 seconds.
+      let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved");
+        }, 6000);
+      });
+  
+      // Console log before calling the promise
+      console.log("Before calling promise");
+  
+      // Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+        console.log("From Callback: " + successMessage);
+  
+        // Make an API call to retrieve the book list
+        axios.get('https://theophilusuz-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai./books')
+          .then(response => {
+            const books = response.data;
+            return res.status(200).json(books);
+          })
+          .catch(error => {
+            return res.status(500).json({ message: "Error retrieving book list" });
+          });
+      });
+  
+      // Console log after calling the promise
+      console.log("After calling promise");
+    } catch (error) {
+      return res.status(500).json({ message: "Error retrieving book list" });
+    }
+  });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try {
+      const isbn = req.params.isbn;
   
+      // Creating a promise method. The promise will get resolved when the timer times out after 6 seconds.
+      let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved");
+        }, 6000);
+      });
+  
+      // Console log before calling the promise
+      console.log("Before calling promise");
+  
+      // Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+        console.log("From Callback: " + successMessage);
+  
+        // Make an API call to retrieve the book details based on ISBN
+        axios.get(`https://theophilusuz-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai./books/${isbn}`)
+          .then(response => {
+            const book = response.data;
+            return res.status(200).json(book);
+          })
+          .catch(error => {
+            return res.status(404).json({ message: "Book not found" });
+          });
+      });
+  
+      // Console log after calling the promise
+      console.log("After calling promise");
+    } catch (error) {
+      return res.status(500).json({ message: "Error retrieving book details" });
+    }
+  });
+
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/author/:author', async function (req, res) {
+    try {
+      const author = req.params.author;
+  
+      // Creating a promise method. The promise will get resolved when the timer times out after 6 seconds.
+      let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved");
+        }, 6000);
+      });
+  
+      // Console log before calling the promise
+      console.log("Before calling promise");
+  
+      // Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+        console.log("From Callback: " + successMessage);
+  
+        // Make an API call to retrieve the book details based on author
+        axios.get(`https://theophilusuz-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/books?author=${author}`)
+          .then(response => {
+            const books = response.data;
+            return res.status(200).json(books);
+          })
+          .catch(error => {
+            return res.status(404).json({ message: "No books found for the given author" });
+          });
+      });
+  
+      // Console log after calling the promise
+      console.log("After calling promise");
+    } catch (error) {
+      return res.status(500).json({ message: "Error retrieving book details" });
+    }
+  });
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book details based on title
+public_users.get('/title/:title', async function (req, res) {
+    try {
+      const title = req.params.title;
+  
+      // Creating a promise method. The promise will get resolved when the timer times out after 6 seconds.
+      let myPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve("Promise resolved");
+        }, 6000);
+      });
+  
+      // Console log before calling the promise
+      console.log("Before calling promise");
+  
+      // Call the promise and wait for it to be resolved and then print a message.
+      myPromise.then((successMessage) => {
+        console.log("From Callback: " + successMessage);
+  
+        // Make an API call to retrieve the book details based on title
+        axios.get(`https://theophilusuz-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/books?title=${title}`)
+          .then(response => {
+            const books = response.data;
+            return res.status(200).json(books);
+          })
+          .catch(error => {
+            return res.status(404).json({ message: "No books found for the given title" });
+          });
+      });
+  
+      // Console log after calling the promise
+      console.log("After calling promise");
+    } catch (error) {
+      return res.status(500).json({ message: "Error retrieving book details" });
+    }
+  });
+
+public_users.get('/review/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  let bookArray = Object.values(books);
+  let book = bookArray.find((b) => b.isbn === isbn);
+  if (book && book.reviews && book.reviews.length > 0) {
+    return res.status(200).json(book.reviews);
+  } else {
+    return res.status(404).json({ message: "No reviews found for the given ISBN" });
+  }
 });
 
 module.exports.general = public_users;
